be035f2cfb24dcc2b417f6ab148fe3163b4ec1da
henriquedelima92 <henriquedelima92@hotmail.com>
2020-05-16 10:27:43 -0300

    Added player projectiles.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,10 +2,14 @@ extends KinematicBody2D
 
 const SPEED = 500
 
+export(PackedScene) var projectile
+
 onready var sprite = $Sprite
+onready var timer = $Timer
 
 var screen_size 
 var playerSize
+var can_shoot = true
 
 func _ready():
 	screen_size = get_viewport_rect().size.x
@@ -17,4 +21,15 @@ func _process(delta):
 	elif Input.is_action_pressed("right"):
 		position.x += SPEED * delta
 		
+	if can_shoot == true and Input.is_action_pressed("shoot"):
+		can_shoot = false		
+		var new_projectile = projectile.instance()
+		new_projectile.position = position
+		get_parent().add_child(new_projectile)
+		timer.start()
+		
 	position.x = clamp(position.x, 0 + (playerSize/2), screen_size-(playerSize/2))
+
+
+func _on_Timer_timeout():
+	can_shoot = true
